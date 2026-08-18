001b73edd0180a54751e3d282f73a13c915ac2af
vkemekv <dldkdud0771@gmail.com>
2026-08-18 16:33:27 +0900

    feat: 3D-1 램프 빛 교체

--- Twofold_TheOtherRoom/Assets/Scripts/3D-1/LAY_Lamp.cs
+++ Twofold_TheOtherRoom/Assets/Scripts/3D-1/LAY_Lamp.cs
@@ -1,51 +1,42 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections; 
+public class LAY_Lamp : MonoBehaviour 
+{ 
+    public Light pointLight; 
+    public Renderer lampRenderer; 
+    Material mat; 
 
-public class LAY_Lamp : MonoBehaviour
-{
-    public Light pointLight;
-    public Renderer lampRenderer;
+    void Awake() 
+    { 
+        mat = lampRenderer.material; 
+        pointLight.enabled = false; 
+        mat.SetColor("_EmissionColor", Color.black); 
+    }
 
-    Material mat;
-
-    void Awake()
+    public void BlinkRed() 
+    { 
+        StopAllCoroutines(); 
+        StartCoroutine(Blink(Color.red)); 
+    }
+    public void BlinkGreen() 
+    { 
+        StopAllCoroutines(); 
+        StartCoroutine(Blink(Color.green)); 
+    } 
+    public void BlinkBlue() 
+    { 
+        StopAllCoroutines(); 
+        StartCoroutine(Blink(Color.blue)); 
+    } 
+    IEnumerator Blink(Color color) 
     {
-        mat = lampRenderer.material;
-
-        pointLight.enabled = false;
-        mat.SetColor("_EmissionColor", Color.black);
-    }
-
-    public void BlinkRed()
-    {   
-        Debug.Log("빨강 반짝");
-        StopAllCoroutines();
-        StartCoroutine(Blink(Color.red));
-    }
-
-    public void BlinkGreen()
-    {
-        StopAllCoroutines();
-        StartCoroutine(Blink(Color.green));
-    }
-
-    public void BlinkBlue()
-    {
-        StopAllCoroutines();
-        StartCoroutine(Blink(Color.blue));
-    }
-
-    IEnumerator Blink(Color color)
-    {
-        Debug.Log("Blink 시작");
-        pointLight.enabled = true;
-        pointLight.color = color;
-
-        mat.SetColor("_EmissionColor", color * 3);
-
-        yield return new WaitForSeconds(0.5f);
-
-        pointLight.enabled = false;
-        mat.SetColor("_EmissionColor", Color.black);
-    }
+        Debug.Log("Blink 시작"); 
+        pointLight.enabled = true; 
+        pointLight.color = color; 
+        
+        mat.SetColor("_EmissionColor", color * 3); 
+        yield return new WaitForSeconds(0.5f); 
+        pointLight.enabled = false; 
+        mat.SetColor("_EmissionColor", Color.black); 
+    } 
 }
