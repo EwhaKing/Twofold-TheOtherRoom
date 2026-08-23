1df8daaa1e37af861cfae0829f73f83bd1d7e1b9
김유민 <kymld0612@gmail.com>
2026-08-24 05:21:16 +0900

    feat: 3D-10 효과음 추가

--- Twofold_TheOtherRoom/Assets/Scripts/3D-10/cylinder3D10_local.cs
+++ Twofold_TheOtherRoom/Assets/Scripts/3D-10/cylinder3D10_local.cs
@@ -20,6 +20,7 @@ public class cylinder3D10_local : MonoBehaviour, IInteractable
         if (isRotating || (controller != null && controller.GetSolve()))
             return;
 
+        SoundManager.Instance.PlaySFX(SFXType.Scrape);
         currentZ = Mathf.Repeat(currentZ + rotationStep, 360f);
         StartCoroutine(RotateZSmooth());
         controller.CheckAnswer(); 
