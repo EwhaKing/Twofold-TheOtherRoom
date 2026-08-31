dc57b4cb6d2f0bf7c6ba0d9960fba88e887cd751
So Eun Sim <silvercowsss@gmail.com>
2026-08-31 11:12:04 +0900

    2D-3
    수정

--- Twofold_TheOtherRoom/Assets/Scripts/2D-3/NetPuzzleDragItem.cs
+++ Twofold_TheOtherRoom/Assets/Scripts/2D-3/NetPuzzleDragItem.cs
@@ -1,4 +1,4 @@
- using UnityEngine;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class NetPuzzleDragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
@@ -32,7 +32,10 @@ public class NetPuzzleDragItem : MonoBehaviour, IBeginDragHandler, IDragHandler,
     public void OnBeginDrag(PointerEventData eventData)
     {
         parentAfterDrag = transform.parent;
+        
+        // 드래그를 시작할 때 어두운 배경에 가려지지 않도록 맨 앞으로 끌어올림
         transform.SetAsLastSibling();
+        
         canvasGroup.blocksRaycasts = false;
     }
 
@@ -45,6 +48,10 @@ public class NetPuzzleDragItem : MonoBehaviour, IBeginDragHandler, IDragHandler,
     {
         transform.SetParent(parentAfterDrag);
         rectTransform.anchoredPosition = Vector2.zero; // 슬롯 중앙 정렬
+        
+        // 슬롯이나 기존 부모로 들어간 뒤에도 발판 뒤로 숨지 않도록 부모 내에서 최상단 배치
+        transform.SetAsLastSibling();
+        
         canvasGroup.blocksRaycasts = true;
     }
 
@@ -53,5 +60,8 @@ public class NetPuzzleDragItem : MonoBehaviour, IBeginDragHandler, IDragHandler,
         parentAfterDrag = startParent;
         transform.SetParent(startParent);
         rectTransform.anchoredPosition = startAnchoredPos;
+        
+        // 원위치로 리셋될 때도 배경 뒤로 가려지지 않게 UI 순서를 맨 앞으로 당김
+        transform.SetAsLastSibling();
     }
 }
--- Twofold_TheOtherRoom/Assets/Scripts/2D-3/PuzzleResetController.cs
+++ Twofold_TheOtherRoom/Assets/Scripts/2D-3/PuzzleResetController.cs
@@ -8,9 +8,21 @@ public class PuzzleResetController : MonoBehaviour
     [Header("게임 매니저 연결")]
     public NetPuzzleGameManager gameManager;
 
+    private void Update()
+    {
+        // 깨지는 연출(breakEffect)이 켜졌는지 감시하다가, 켜지면 리셋 버튼을 스스로 숨깁니다.
+        if (gameManager != null && gameManager.breakEffect != null)
+        {
+            if (gameManager.breakEffect.gameObject.activeSelf)
+            {
+                HideResetButton();
+            }
+        }
+    }
+
     public void ResetPuzzle()
     {
-        // 1. 씬 내의 모든 드래그 아이템 원위치 복원
+        // 1. 씬 내의 모든 드래그 아이템 원위치 복원 및 맨 앞으로 배치
         if (allDragItems != null)
         {
             foreach (NetPuzzleDragItem item in allDragItems)
@@ -18,6 +30,8 @@ public class PuzzleResetController : MonoBehaviour
                 if (item != null)
                 {
                     item.ResetToOriginalPosition();
+                    // 발판 뒤로 숨는 현상 방지를 위해 UI 순서를 맨 앞으로 당김
+                    item.transform.SetAsLastSibling(); 
                 }
             }
         }
@@ -35,5 +49,11 @@ public class PuzzleResetController : MonoBehaviour
         }
 
         Debug.Log("모든 퍼즐 아이템이 원위치로 리셋되었습니다.");
+    }
+
+    // 리셋 버튼 자체를 화면에서 숨기는 기능
+    public void HideResetButton()
+    {
+        gameObject.SetActive(false);
     }
 }
